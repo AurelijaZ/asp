bda1c2426798013e340a51b1d032b8b886df28cb
Aurelijaz <aurelijazuba@gmail.com>
2016-09-01 16:22:19 +0100

    ...

---
+++ MVCMovie/MVCMovie/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿
+using MVCMovie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private MoviesDBAurelijaEntities db = new MoviesDBAurelijaEntities();
+
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var movies = from m in db.Movies
+                         select m;
+
+            return View();
+        }
+    }
+}
